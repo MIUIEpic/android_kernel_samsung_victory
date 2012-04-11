d04f0b12fad90c633c9948291870611a07551846
Ezekeel <notezekeel@googlemail.com>
2012-04-11 01:32:31 -0400

    Add BLQ
    Conflicts:
    
    	Kernel/drivers/misc/Kconfig
    	Kernel/drivers/misc/Makefile
    	Kernel/include/linux/blx.h

--- Kernel/include/linux/blx.h
+++ Kernel/include/linux/blx.h
@@ -4,7 +4,11 @@
 #define _LINUX_BLX_H
 
 // for kernels including the 100% charging 'fix' change this to 100:
+<<<<<<< HEAD
 #define MAX_CHARGINGLIMIT 100
+=======
+#define MAX_CHARGINGLIMIT 96
+>>>>>>> 3f66321... Add BLQ
 
 int get_charginglimit(void);
 

b51ee9f7b0ee44d5a83fd1a932f7b6b67fd77f2d
Jake Weinstein <xboxfanj@msn.com>
2012-08-28 16:35:22 -0500

    Fix BLX error

--- Kernel/include/linux/blx.h
+++ Kernel/include/linux/blx.h
@@ -4,11 +4,7 @@
 #define _LINUX_BLX_H
 
 // for kernels including the 100% charging 'fix' change this to 100:
-<<<<<<< HEAD
 #define MAX_CHARGINGLIMIT 100
-=======
-#define MAX_CHARGINGLIMIT 96
->>>>>>> 3f66321... Add BLQ
 
 int get_charginglimit(void);
 
